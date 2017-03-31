f62f58b3e7f7787b83b8f350694e1242e76188a8
Caleb Kapusta <calebk@magenic.net>
2017-03-31 12:35:12 -0700

    Add project files.

---
+++ mvcPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcPractice.Startup))]
+namespace mvcPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
