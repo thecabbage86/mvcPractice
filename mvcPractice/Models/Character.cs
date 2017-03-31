b024e11e48f2e3aeaa625cefcc19fb6fde96abe8
Caleb Kapusta <calebk@magenic.net>
2017-03-31 14:32:50 -0700

    Add initial character controller, model, and view

---
+++ mvcPractice/Models/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvcPractice.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        //public CharacterStats Stats { get; set; }
+    }
+
+    public class CharacterStats
+    {
+        public int Health { get; set; }
+        public int Intelligence { get; set; }
+        public int Vitality { get; set; }
+        public int Strength { get; set; }
+        public int Mind { get; set; }
+    }
+
+    public class CharacterDBContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+    }
+}
